8be0954b4f09bce266bb741dbda39a7c51b930f3
kevo-1 <kevin.bastawrous@gmail.com>
2026-01-10 01:13:21 +0200

    feat(resolver): implemented the basic caching for resolver

---
+++ core/resolver.go
@@ -0,0 +1,48 @@
+package core
+
+import (
+	models "DNS-server/models"
+	"strings"
+	"sync"
+)
+
+type Resolver struct {
+	cache map[string]string
+	mu    sync.Mutex
+}
+
+func (r *Resolver) lookupCache(domain string) (string, bool){
+	//lock the cache as it is a critical section
+	r.mu.Lock()
+	defer r.mu.Unlock()
+
+	if ip, ok := r.cache[domain]; ok {
+		return ip, true
+	}
+	return "", false
+}
+
+var (
+    instance *Resolver
+    once     sync.Once
+)
+
+func GetInstance() *Resolver {
+    once.Do(func() {
+        instance = &Resolver{
+            cache: make(map[string]string),
+        }
+    })
+    return instance
+}
+
+func retrieveIP(url models.URL) (string, error){
+	resolver := GetInstance()
+	domain := strings.Split(url.Authority.Host, ".")
+
+	if ip, ok := resolver.lookupCache(domain[len(domain)-1]); ok {
+		return ip, nil
+	}
+
+	return "", nil
+}
